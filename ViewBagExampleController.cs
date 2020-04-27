f3462e7d3032c7a7f0ad894ce58aedc7e905f361
karravularajeswari <43335512+karravularajeswari@users.noreply.github.com>
2020-04-27 14:25:13 +0530

    Add files via upload

---
+++ ViewBagExampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class ViewBagExampleController : Controller
+    {
+        // GET: ViewBagExample
+        public ActionResult Index()
+        {
+            ViewBag.Uname = "RP";
+            ViewBag.Categories = new List<string>()
+            {
+                "Electronics",
+                "Shoes"
+            };
+            return View();
+        }
+    }
+}
